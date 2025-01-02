755d2de6853efb2f18eb6ba5539315cb1d39e45d
duygusahan <103382151+duygusahan@users.noreply.github.com>
2025-01-02 22:14:12 +0300

    ...

---
+++ HotelProject.WebUi/ViewComponents/AdminLayoutComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUi.ViewComponents.AdminLayoutComponents
+{
+	public class _AdminLayoutHeaderComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
